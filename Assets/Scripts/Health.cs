eb7a324ea25a7d52a179d6f683101ee31571fd64
Viktorthor <viktorthorr@gmail.com>
2021-07-05 10:51:07 +0000

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+    public int health = 10;
+    public TextMeshProUGUI healthDisplay;
+
+    PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    public void TakeDamage()
+    {
+        view.RPC("TakeDamageRPC", RpcTarget.All);
+    }
+    [PunRPC]
+    void TakeDamageRPC()
+    {
+        health -= 1;
+        healthDisplay.text = health.ToString();
+    }
+}
