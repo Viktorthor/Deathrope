9c3b27672f09335519f144a3970a68cb6121a268
Viktorthor <viktorthorr@gmail.com>
2021-09-03 09:44:08 +0000

    Screen wrap now active along with dash feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ using Photon.Pun;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    float orgSpeed;
+    public float dashSpeed;
+    public float dashTime;
+    public float minX, maxX, minY, maxY;
+
     PhotonView view;
     Animator anim;
     Health healthScript;
@@ -13,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        orgSpeed = speed;
         view = GetComponent<PhotonView>();
         anim = GetComponent<Animator>();
         healthScript = FindObjectOfType<Health>();
@@ -26,6 +32,13 @@ public class PlayerController : MonoBehaviour
             Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
             transform.position += (Vector3)moveAmount;
+
+            if (Input.GetKeyDown(KeyCode.Space) && moveInput != Vector2.zero)
+            {
+                StartCoroutine(Dash());
+            }
+
+            screenWrap();
 
             if(moveInput == Vector2.zero)
             {
@@ -42,6 +55,36 @@ public class PlayerController : MonoBehaviour
             rend.SetPosition(1, transform.position);
         }
         
+    }
+
+    void screenWrap()
+    {
+        if(transform.position.x < minX)
+        {
+            transform.position = new Vector2(maxX, transform.position.y);
+        }
+
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector2(minX, transform.position.y);
+        }
+
+        if (transform.position.y < minY)
+        {
+            transform.position = new Vector2(transform.position.x, maxY);
+        }
+
+        if (transform.position.y > maxY)
+        {
+            transform.position = new Vector2(transform.position.x, minY);
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        speed = dashSpeed;
+        yield return new WaitForSeconds(dashTime);
+        speed = orgSpeed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
